ab662303bd2587287bd10712af248668f89a5db4
oa9bata <batawelofficial@gmail.com>
2025-07-04 01:16:09 -0500

    keys

--- config.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_CONFIG = {
-    url: 'https://trootamfcuketowpeyvi.supabase.co',  // Replace with your Project URL
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyb290YW1mY3VrZXRvd3BleXZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE2MDc2NjgsImV4cCI6MjA2NzE4MzY2OH0.T2seLyR_wI8FlLXk7Mv806IZ6r1ODiMcTEca5rXd8Rk'                 // Replace with your anon/public key
-};
